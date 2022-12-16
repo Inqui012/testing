c4475747cc9a5eef54afc58e6b2cab122d96521e
Inqui012 <xii0712@gmail.com>
2022-12-16 16:41:57 +0900

    first

---
+++ src/jsx/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../scss/common.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h1>Testing</h1>
+                <nav>
+                    <ul>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
