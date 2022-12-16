ca09346afab7a366eb09a4f58f3fcb65a6015db2
Inqui012 <xii0712@gmail.com>
2022-12-16 18:26:14 +0900

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './jsx/header';
+import Main from './jsx/main'
 
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
 
---
+++ src/jsx/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../scss/main.scss';
+
+const Main = () => {
+    return (
+        <main>
+            <section>
+                <div>
+                    <div>
+                        img
+                    </div>
+                    <div>
+                        <div>옵션선택</div>
+                        <div>총가격</div>
+                        <div>
+                            <button>장바구니</button>
+                            <button>구매</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    상품상세페이지
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default Main;
